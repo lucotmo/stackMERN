5181bc597ca557363f10b8399f1b6c39d311f1ab
lucotmo <lucotmo@gmail.com>
2020-01-04 17:21:45 -0500

    workflow backend

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello world'
+  })
+})
+
+const schema = {};
+
+app.use('/graphql', graphqlHTTP({
+  graphiql: true,
+  schema: schema
+}));
+
+app.listen(3000, () => console.log('Server on port 3000'))
